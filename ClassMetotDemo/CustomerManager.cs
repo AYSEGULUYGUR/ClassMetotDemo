50da54e26dfaf9d84b6eb3875fbb7c2a9744d28e
Ayşegül UYĞUR <ayseguluygur54@gmail.com>
2021-01-25 20:45:07 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+        public void List(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("MÜŞTERİNİN ADI :" +customer.customerName);
+                Console.WriteLine("MÜŞTERİNİN SOYADI :" + customer.surname);
+                Console.WriteLine("MÜŞTERİNİN ID'Sİ :" + customer.Id);
+            }
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.customerName = "Ayşe";
+            customer.surname = "Yayla";
+            customer.Id = 56985;
+
+            Customer customer1 = new Customer();
+            customer1.customerName = "Fatma";
+            customer1.surname = "Uygur";
+            customer1.Id = 87465;
+
+            Customer[] customers = new Customer[2];
+            customers[0] = customer;
+            customers[1] = customer1;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.Update(customer1);
+            customerManager.List(customers);
+            customerManager.Delete(customer);
+
+
+
+
+        }
+    }
+}
